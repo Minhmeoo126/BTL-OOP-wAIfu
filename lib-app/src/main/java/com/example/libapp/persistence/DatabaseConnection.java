08dfbdf54898f7f3920a44b668b7d4b2247a9955
Dương Quang Minh <24022401@.vnu.edu.vn>
2025-03-27 15:16:29 +0700

    "Update DatabaseConnection"

--- lib-app/src/main/java/com/example/libapp/persistence/DatabaseConnection.java
+++ lib-app/src/main/java/com/example/libapp/persistence/DatabaseConnection.java
@@ -1,4 +1,27 @@
 package com.example.libapp.persistence;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/lib"; //
+    private static final String USER = "root";  //
+    private static final String PASSWORD = "meomeomeo";
+
+    public static Connection connect() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println(" Kết nối thành công!");
+        } catch (SQLException e) {
+            System.out.println(" Kết nối thất bại!");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void main(String[] args) {
+        connect(); // Test kết nối
+    }
 }
