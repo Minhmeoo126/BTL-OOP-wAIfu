504dec6f40120a12a0430fa900442f05da6d8ef5
Dương Quang Minh <24022401@.vnu.edu.vn>
2025-03-27 22:39:47 +0700

    Update model

--- lib-app/src/main/java/com/example/libapp/model/Book.java
+++ lib-app/src/main/java/com/example/libapp/model/Book.java
@@ -1,4 +1,101 @@
 package com.example.libapp.model;
 
+import java.sql.Timestamp;
+
 public class Book {
+    private int id;
+    private String title;
+    private int authorId;
+    private int categoryId;
+    private String genre;
+    private int publicationYear;
+    private int availableCopies;
+    private int totalCopies;
+    private Timestamp createdAt;
+
+    public Book(int id, String title, int authorId, int categoryId, String genre, int publicationYear, int availableCopies, int totalCopies, Timestamp createdAt) {
+        this.id = id;
+        this.title = title;
+        this.authorId = authorId;
+        this.categoryId = categoryId;
+        this.genre = genre;
+        this.publicationYear = publicationYear;
+        this.availableCopies = availableCopies;
+        this.totalCopies = totalCopies;
+        this.createdAt = createdAt;
+    }
+
+    // Getters và Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        this.availableCopies = availableCopies;
+    }
+
+    public int getTotalCopies() {
+        return totalCopies;
+    }
+
+    public void setTotalCopies(int totalCopies) {
+        this.totalCopies = totalCopies;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
 }
