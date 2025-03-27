ffdb7fe67f1e6fbf46e49f271ac320f63adc6276
Dương Quang Minh <24022401@.vnu.edu.vn>
2025-03-27 22:39:47 +0700

    Update model

--- lib-app/src/main/java/com/example/libapp/model/BorrowingRecord.java
+++ lib-app/src/main/java/com/example/libapp/model/BorrowingRecord.java
@@ -1,4 +1,81 @@
 package com.example.libapp.model;
 
+import java.sql.Date;
+
 public class BorrowingRecord {
+    private int id;
+    private int userId;
+    private int bookId;
+    private Date borrowDate;
+    private Date dueDate;
+    private Date returnDate;
+    private String status;
+
+    public BorrowingRecord(int id, int userId, int bookId, Date borrowDate, Date dueDate, Date returnDate, String status) {
+        this.id = id;
+        this.userId = userId;
+        this.bookId = bookId;
+        this.borrowDate = borrowDate;
+        this.dueDate = dueDate;
+        this.returnDate = returnDate;
+        this.status = status;
+    }
+
+    // Getters và Setters
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Date getBorrowDate() {
+        return borrowDate;
+    }
+
+    public void setBorrowDate(Date borrowDate) {
+        this.borrowDate = borrowDate;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
